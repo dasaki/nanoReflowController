61c8698942013a244e8d22d8e8c7169a2af1d013
J. R. Schmid <jrs+git@weitnahbei.de>
2019-01-24 17:19:26 +0100

    Clean up a little bit.

--- ReflowController/PDQ_ST7735_config.h
+++ ReflowController/PDQ_ST7735_config.h
@@ -10,14 +10,14 @@
 // NOTE: The tab colors refer to Adafruit versions, other suppliers may vary (you may have to experiment to find the right one).
 enum
 {
-  ST7735_INITB             = 0,        // 1.8" (128x160) ST7735B chipset (only one type)
-  ST7735_INITR_GREENTAB    = 1,        // 1.8" (128x160) ST7735R chipset with green tab (same as ST7735_INITR_18GREENTAB)
-  ST7735_INITR_REDTAB      = 2,        // 1.8" (128x160) ST7735R chipset with red tab (same as ST7735_INITR_18REDTAB)
-  ST7735_INITR_BLACKTAB    = 3,        // 1.8" (128x160) ST7735S chipset with black tab (same as ST7735_INITR_18BLACKTAB)
-  ST7735_INITR_144GREENTAB = 4,        // 1.4" (128x128) ST7735R chipset with green tab
-  ST7735_INITR_18GREENTAB  = ST7735_INITR_GREENTAB,  // 1.8" (128x160) ST7735R chipset with green tab
-  ST7735_INITR_18REDTAB    = ST7735_INITR_REDTAB,    // 1.8" (128x160) ST7735R chipset with red tab
-  ST7735_INITR_18BLACKTAB  = ST7735_INITR_BLACKTAB,  // 1.8" (128x160) ST7735S chipset with black tab
+  ST7735_INITB             = 0,                     // 1.8"  (128x160) ST7735B chipset (only one type)
+  ST7735_INITR_GREENTAB    = 1,                     // 1.8"  (128x160) ST7735R chipset with green tab (same as ST7735_INITR_18GREENTAB)
+  ST7735_INITR_REDTAB      = 2,                     // 1.8"  (128x160) ST7735R chipset with red tab (same as ST7735_INITR_18REDTAB)
+  ST7735_INITR_BLACKTAB    = 3,                     // 1.8"  (128x160) ST7735S chipset with black tab (same as ST7735_INITR_18BLACKTAB)
+  ST7735_INITR_144GREENTAB = 4,                     // 1.44" (128x128) ST7735R chipset with green tab
+  ST7735_INITR_18GREENTAB  = ST7735_INITR_GREENTAB, // 1.8"  (128x160) ST7735R chipset with green tab
+  ST7735_INITR_18REDTAB    = ST7735_INITR_REDTAB,   // 1.8"  (128x160) ST7735R chipset with red tab
+  ST7735_INITR_18BLACKTAB  = ST7735_INITR_BLACKTAB, // 1.8"  (128x160) ST7735S chipset with black tab
 };
 
 #define ST7735_CHIPSET    ST7735_INITR_REDTAB // <= Set ST7735 LCD chipset/variation here (from above list)
@@ -41,12 +41,12 @@ enum
 //
 //  * = Typically only 3.3V safe logic-line (unless board has level converter [ala Adafruit]). Be careful with 5V!
 
-#define ST7735_CS_PIN   PIN_LCD_CS      // <= /CS pin (chip-select, LOW to get attention of ST7735, HIGH and it ignores SPI bus)
-#define ST7735_DC_PIN   PIN_LCD_DC     // <= DC pin (1=data or 0=command indicator line) also called RS
+#define ST7735_CS_PIN   PIN_LCD_CS  // <= /CS pin (chip-select, LOW to get attention of ST7735, HIGH and it ignores SPI bus)
+#define ST7735_DC_PIN   PIN_LCD_DC  // <= DC pin (1=data or 0=command indicator line) also called RS
 #define ST7735_RST_PIN  PIN_LCD_RST
 // (other pins used are dictated by AVR HW SPI used as shown above)
 
 // other PDQ library options
-#define ST7735_SAVE_SPCR  1     // <= 0/1 with 1 to save/restore AVR SPI control register (to "play nice" when other SPI use)
+#define ST7735_SAVE_SPCR 1 // <= 0/1 with 1 to save/restore AVR SPI control register (to "play nice" when other SPI use)
 
 #endif // PDQ_ST7735_CONFIG
